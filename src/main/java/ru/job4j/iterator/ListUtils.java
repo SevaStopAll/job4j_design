dd76346d704dce14b764513c290cb3a98e39cb64
SevaStopAll <Sevastopallrh@yandex.ru>
2022-12-27 14:10:14 +0400

    7. ListIterator [#350217]

--- src/main/java/ru/job4j/iterator/ListUtils.java
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -18,18 +18,42 @@ public class ListUtils {
     }
 
     public static <T> void addAfter(List<T> list, int index, T value) {
-
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (iterator.previousIndex() == index) {
+                iterator.add(value);
+                break;
+            }
+            iterator.next();
+        }
     }
 
     public static <T> void removeIf(List<T> list, Predicate<T> filter) {
-
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (filter.test(iterator.next())) {
+                iterator.remove();
+            }
+        }
     }
 
     public static <T> void replaceIf(List<T> list, Predicate<T> filter, T value) {
-
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (filter.test(iterator.next())) {
+                iterator.set(value);
+            }
+        }
     }
 
     public static <T> void removeAll(List<T> list, List<T> elements) {
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (elements.contains(iterator.next())) {
+                iterator.remove();
+            }
+        }
 
     }
 
--- src/test/java/ru/job4j/iterator/ListUtilsTest.java
+++ src/test/java/ru/job4j/iterator/ListUtilsTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.*;
+import java.util.function.Predicate;
 
 class ListUtilsTest {
 
@@ -31,5 +32,25 @@ class ListUtilsTest {
     void whenAddAfter() {
         ListUtils.addAfter(input, 0, 2);
         assertThat(input).hasSize(3).containsSequence(1, 2, 3);
+    }
+
+    @Test
+    void whenRemoveIf() {
+        Predicate<Integer> pred = (a) -> a < 2;
+        ListUtils.removeIf(input, pred);
+        assertThat(input).containsExactly(3);
+    }
+
+    @Test
+    void whenReplaceIf() {
+        Predicate<Integer> pred = (a) -> a < 2;
+        ListUtils.replaceIf(input, pred, 5);
+        assertThat(input).hasSize(2).containsSequence(5, 3);
+    }
+
+    @Test
+    void whenRemoveAll() {
+        ListUtils.removeAll(input, Arrays.asList(1, 3));
+        assertThat(input).isEmpty();
     }
 }
