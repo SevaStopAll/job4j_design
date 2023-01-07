8ece89f3ea4a19ad3a4125426e02f4c205afaeb3
SevaStopAll <Sevastopallrh@yandex.ru>
2023-01-07 10:32:48 +0400

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+                for (int i = 0; i < 5; i++) {
+                    for (int j = 0; j < 5; j++) {
+                        String e = (i + 1) * (j + 1) + " ";
+                        out.write(e.getBytes());
+                    }
+                    out.write(System.lineSeparator().getBytes());
+                }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
