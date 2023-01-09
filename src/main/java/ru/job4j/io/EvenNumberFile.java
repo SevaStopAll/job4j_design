0058226454f125ae077536ff049b7b3ada55c400
SevaStopAll <Sevastopallrh@yandex.ru>
2023-01-09 19:49:32 +0400

    IO

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -1,23 +1,7 @@
 package ru.job4j.io;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-
 public class EvenNumberFile {
     public static void main(String[] args) {
-        try (FileInputStream in = new FileInputStream("even.txt")) {
-            StringBuilder text = new StringBuilder();
-            int read;
-            while ((read = in.read()) != -1) {
-                text.append((char) read);
-            }
-            String[] lines = text.toString().split(System.lineSeparator());
-            for (String line : lines) {
-                boolean rsl = Integer.parseInt(line) % 2 == 0;
-                System.out.println(rsl);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
     }
 }
---
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -0,0 +1,18 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("input.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+            System.out.println(text);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
