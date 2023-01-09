bb582efd66109d41dbbd5d0831209e09a578b03a
Sevastopall <Sevastopallrh@yandex.ru>
2023-01-09 07:41:54 +0400

    0.2. FileInputStream [#4898]

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 public class EvenNumberFile {
     public static void main(String[] args) {
-        try (FileInputStream in = new FileInputStream("even.txt")){
+        try (FileInputStream in = new FileInputStream("even.txt")) {
             StringBuilder text = new StringBuilder();
             int read;
             while ((read = in.read()) != -1) {
