4a975fa6ca25478f257e581c17f50ffd77223f92
SevaStopAll <Sevastopallrh@yandex.ru>
2023-01-10 19:41:24 +0400

    4.0. File [#252491]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -1,20 +1,19 @@
 package ru.job4j.io;
 
 import java.io.File;
+import java.util.Objects;
 
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("c:\\projects");
+        File file = new File("C:\\Users\\Мы\\IdeaProjects");
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
         if (!file.isDirectory()) {
             throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
-        System.out.println(String.format("size : %s", file.getTotalSpace()));
-        for (File subfile : file.listFiles()) {
-            System.out.print(subfile.getAbsoluteFile() + " ");
-            System.out.println(subfile.getAbsoluteFile().length());
+        for (File subfile : Objects.requireNonNull(file.listFiles())) {
+            System.out.println(subfile.getName() + " " + subfile.getName().length());
         }
     }
 }
