e9b9c813ada40698d12e2f3309ce000cc6bbd5dd
SevaStopAll <Sevastopallrh@yandex.ru>
2023-01-10 20:00:05 +0400

    4.1. Сканирование файловой системы. [#106929]

---
+++ src/main/java/ru/job4j/io/Search.java
@@ -0,0 +1,21 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Search {
+    public static void main(String[] args) throws IOException {
+        Path start = Paths.get(".");
+        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+    }
+
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        SearchFiles searcher = new SearchFiles(condition);
+        Files.walkFileTree(root, searcher);
+        return null;
+    }
+}
---
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -0,0 +1,37 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.function.Predicate;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+public class SearchFiles implements FileVisitor<Path> {
+
+    public SearchFiles(Predicate<Path> condition) {
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        return null;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        System.out.println(file.toAbsolutePath());
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return null;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        return null;
+    }
+}
