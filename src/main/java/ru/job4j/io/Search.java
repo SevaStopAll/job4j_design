830a4bf1a336ff9fda11483fe274e05e39dd7fa0
SevaStopAll <Sevastopallrh@yandex.ru>
2023-01-10 20:20:55 +0400

    4.1. Сканирование файловой системы. [#106929]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -16,6 +16,6 @@ public class Search {
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
-        return null;
+        return searcher.getPaths;
     }
 }
