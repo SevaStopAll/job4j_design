c408251f0bd0cda140edf16a8d118892024e8a3c
SevaStopAll <Sevastopallrh@yandex.ru>
2023-01-11 20:56:06 +0400

    4.1. Сканирование файловой системы. [#106929]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -10,11 +10,11 @@ import java.util.function.Predicate;
 public class Search {
     public static void main(String[] args) throws IOException {
         Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        search(start, p -> p.toFile().getName().endsWith(".xml")).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
-        SearchFiles searcher = new SearchFiles();
+        SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
     }
--- src/main/java/ru/job4j/io/SearchFiles.java
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -2,27 +2,30 @@ package ru.job4j.io;
 
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
-
-import static java.nio.file.FileVisitResult.CONTINUE;
+import java.util.stream.Collectors;
 
 public class SearchFiles extends SimpleFileVisitor<Path> {
-    List<Path> found;
+    List<Path> found = new ArrayList<>();
     Predicate<Path> pred;
+
+    public SearchFiles(Predicate<Path> condition) {
+        this.pred = condition;
+    }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        found.add(file.getFileName());
+        found.add(file);
         return FileVisitResult.CONTINUE;
     }
 
-    public List<Path> getPaths() {
-        return found.stream().filter(pred).toList();
+    public  List<Path> getPaths() {
+        return found.stream().filter(pred).collect(Collectors.toList());
     }
 }
 
