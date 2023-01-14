9f96ced2a1ced5b754ff4bc1c66b6ccd9604027e
SevaStopAll <Sevastopallrh@yandex.ru>
2023-01-14 17:35:20 +0400

    Поиск дубликатов [#315066 #380302]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -7,31 +7,19 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    private List<Path> found = new ArrayList<>();
     private HashMap<FileProperty, ArrayList<Path>> duplicates = new HashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        found.add(file);
+        FileProperty fileProperty = new FileProperty(file.toFile().length(), file.getFileName().toString());
+        duplicates.putIfAbsent(fileProperty, new ArrayList<>());
+        duplicates.get(fileProperty).add(file);
         return super.visitFile(file, attrs);
     }
 
-    private void findDuplicates() {
-        for (Path file : found) {
-            if (duplicates.containsKey(new FileProperty(file.toFile().length(), file.getFileName().toString()))) {
-                duplicates.get(new FileProperty(file.toFile().length(), file.getFileName().toString())).add(file);
-            } else {
-                duplicates.put(new FileProperty(file.toFile().length(), file.getFileName().toString()), new ArrayList<>());
-                duplicates.get(new FileProperty(file.toFile().length(), file.getFileName().toString())).add(file);
-            }
-        }
-    }
-
     public void getDuplicates() {
-        findDuplicates();
         for (FileProperty file : duplicates.keySet()) {
             if (duplicates.get(file).size() > 1) {
                 System.out.printf("%s, %d %n", file.getName(), file.getSize());
