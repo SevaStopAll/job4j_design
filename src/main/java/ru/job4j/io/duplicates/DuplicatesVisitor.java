5b3b62e378ac4a2844dd3717e0c525b74b7821fe
SevaStopAll <Sevastopallrh@yandex.ru>
2023-01-14 15:08:59 +0400

    Поиск дубликатов [#315066 #380302]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -10,20 +10,33 @@ import java.util.HashMap;
 import java.util.List;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-
-    private HashMap<FileProperty, List<Path>> found = new HashMap<>();
+    private List<Path> found = new ArrayList<>();
+    private HashMap<FileProperty, ArrayList<Path>> duplicates = new HashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        FileProperty key = new FileProperty(file.toFile().length(), file.getFileName().toString());
-        found.put(key, new ArrayList<>());
-        if (found.containsKey(key)) {
-            found.get(key).add(file);
-        }
+        found.add(file);
         return super.visitFile(file, attrs);
     }
 
+    private void findDuplicates() {
+        for (Path file : found) {
+            if (duplicates.containsKey(new FileProperty(file.toFile().length(), file.getFileName().toString()))) {
+                duplicates.get(new FileProperty(file.toFile().length(), file.getFileName().toString())).add(file);
+            } else {
+                duplicates.put(new FileProperty(file.toFile().length(), file.getFileName().toString()), new ArrayList<>());
+                duplicates.get(new FileProperty(file.toFile().length(), file.getFileName().toString())).add(file);
+            }
+        }
+    }
+
     public void getDuplicates() {
-        found.keySet().stream().filter(k -> !found.get(k).contains(null)).forEach(f -> System.out.printf("%s, %d, %n", f.getName(), f.getSize()));
+        findDuplicates();
+        for (FileProperty file : duplicates.keySet()) {
+            if (duplicates.get(file).size() > 1) {
+                System.out.printf("%s, %d %n", file.getName(), file.getSize());
+                duplicates.get(file).forEach(k -> System.out.printf("%s %n", k.toAbsolutePath().normalize()));
+            }
+        }
     }
 }
