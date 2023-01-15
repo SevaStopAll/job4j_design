01a5b3b1bf29c2c810f357233325a5244a61fe17
SevaStopAll <Sevastopallrh@yandex.ru>
2023-01-15 23:56:00 +0400

    5.2. Архивировать проект [#861]

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## О проекте
 
-I don't know about what will be this project, 
+I don't know about what this project will be, 
 but I think it will be about upgrade our
 skills to Junior Developer level. 
 As for me, I am ready to learn as much, 
--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,44 +1,45 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
-    public void packFiles(List<File> sources, File target) throws IOException {
+    public void packFiles(List<File> sources, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (File file : sources) {
+                zip.putNextEntry(new ZipEntry(file.getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(file))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
     }
 
     public void packSingleFile(File source, File target) {
-        String filename = source.getName();
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target.getName())));
-            FileInputStream fis = new FileInputStream(filename)) {
-            ZipEntry entry1 = new ZipEntry(source.getName());
-            zip.putNextEntry(entry1);
-            // считываем содержимое файла в массив byte
-            byte[] buffer = new byte[fis.available()];
-            fis.read(buffer);
-            // добавляем содержимое к архиву
-            zip.write(buffer);
-            // закрываем текущую запись для новой записи
-            zip.closeEntry();
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            zip.putNextEntry(new ZipEntry(source.getPath()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
     public static void main(String[] args) throws IOException {
-        ArgsName jvm = ArgsName.of(args);
         Zip zip = new Zip();
-        /*zip.packSingleFile(new File("./checkstyle.xml"), new File(jvm.get("o")));*/
+        ArgsName jvm = ArgsName.of(args);
         List<File> list = Search.search(Path.of(jvm.get("d")), f ->  !f.endsWith(jvm.get("e"))).stream().map(Path::toFile).toList();
         zip.packFiles(
                 list,
                 new File(jvm.get("o")
-        ));
+                ));
     }
 }
+
