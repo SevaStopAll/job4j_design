77cad0330dd04245cd49d2592f8a675ecb581aca
Sevastopall <Sevastopallrh@yandex.ru>
2023-01-17 07:28:30 +0400

    5.2. Архивировать проект [#861 #381555]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -8,6 +8,25 @@ import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
+    private static void validate(String[] args) {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("The launch requires 3 parameters");
+        }
+        File file = new File(args[0].substring(3));
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        if (!args[1].substring(3).startsWith(".") || args[1].substring(3).length() < 2) {
+            throw new IllegalArgumentException(String.format("Not an extension %s", args[1].substring(3)));
+        }
+        if (!args[2].endsWith(".zip")) {
+            throw new IllegalArgumentException(String.format("Not a ZIP file %s", args[2].substring(3)));
+        }
+    }
+
     public void packFiles(List<File> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
             for (File file : sources) {
@@ -16,9 +35,9 @@ public class Zip {
                     zip.write(out.readAllBytes());
                 }
             }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void packSingleFile(File source, File target) {
@@ -33,6 +52,7 @@ public class Zip {
     }
 
     public static void main(String[] args) throws IOException {
+        validate(args);
         Zip zip = new Zip();
         ArgsName jvm = ArgsName.of(args);
         List<File> list = Search.search(Path.of(jvm.get("d")), f ->  !f.endsWith(jvm.get("e"))).stream().map(Path::toFile).toList();
