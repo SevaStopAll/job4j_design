948776dfeb9b92723d49f4ec390e45ee9ee62389
SevaStopAll <Sevastopallrh@yandex.ru>
2023-01-23 21:35:59 +0400

    2. Что такое Socket? [#4850] Added a simple Client Server communication with curl. Now server is answering to a client.

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,6 +3,7 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+
 public class EchoServer {
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
@@ -12,12 +13,15 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        if (str.contains("msg=Bye")) {
-                            server.close();
-                        }
-                        System.out.println(str);
+                    String string = in.readLine().split(" ")[1].substring(6);
+                    if (string.equals("Hello")) {
+                        string = "Hello";
                     }
+                    if (string.equals("Exit")) {
+                        server.close();
+                    }
+                    System.out.println(string);
+                    out.write(string.getBytes());
                     out.flush();
                 }
             }
