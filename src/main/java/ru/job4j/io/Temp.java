49f96f9f97e80bcef573865a5a5a9062ba267b9d
SevaStopAll <Sevastopallrh@yandex.ru>
2023-01-24 05:44:32 +0400

    2. Что такое Socket? [#4850 #384260] Delete for loop.

--- src/main/java/ru/job4j/io/Temp.java
+++ src/main/java/ru/job4j/io/Temp.java
@@ -1,10 +1,13 @@
 package ru.job4j.io;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class EchoServer {
+public class Temp {
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
@@ -13,11 +16,15 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    String str = in.readLine();
-                        if (str.contains("msg=Bye")) {
-                            server.close();
-                        }
-                    System.out.println(str);
+                    String string = in.readLine().split(" ")[1].substring(6);
+                    if (string.equals("Hello")) {
+                        string = "Hello";
+                    }
+                    if (string.equals("Exit")) {
+                        server.close();
+                    }
+                    System.out.println(string);
+                    out.write(string.getBytes());
                     out.flush();
                 }
             }
