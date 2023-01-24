923fc9436fac55b9b7e5fd00c52dbe5542f6976b
SevaStopAll <Sevastopallrh@yandex.ru>
2023-01-24 05:49:06 +0400

    3. Бот [#7921 #384289] Corrected code with final value checking.

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,10 +1,14 @@
 package ru.job4j.io;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
@@ -13,11 +17,15 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    String str = in.readLine();
-                        if (str.contains("msg=Bye")) {
-                            server.close();
-                        }
-                    System.out.println(str);
+                    String string = in.readLine().split(" ")[1].substring(6);
+                    if ("Hello".equals(string)) {
+                        string = "Hello";
+                    }
+                    if ("Exit".equals(string)) {
+                        server.close();
+                    }
+                    System.out.println(string);
+                    out.write(string.getBytes());
                     out.flush();
                 }
             }
