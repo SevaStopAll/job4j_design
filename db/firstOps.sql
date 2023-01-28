b16b2fa124739730399b8a0609715899bf080395
SevaStopAll <Sevastopallrh@yandex.ru>
2023-01-28 09:46:32 +0400

    0. Что такое база данных. [#6860] My first SQL query in this course. I have to learn it once again to gain success.

--- db/firstOps.sql
+++ db/firstOps.sql
@@ -1,5 +1,5 @@
-create table students(id serial primary key, name varchar(255),	surname varchar(255), age smallint );
-insert into students(name, surname, age) values ('Alex', 'Smith', 25);
+create table students(id serial primary key, name varchar(255),	surname varchar(255), age smallint, count numeric);
+insert into students(name, surname, age) values ('Alex', 'Smith', 25, 2200.0);
 select * from students;
 update students set name = 'Nick';
 delete from students;
