b672f58e61ae7e8fe79195e3f72e89165bd56e28
SevaStopAll <Sevastopallrh@yandex.ru>
2023-01-29 20:24:25 +0400

    0. Select [#6861 #386018] Change Date format '1950-01-01'

--- db/fauna.sql
+++ db/fauna.sql
@@ -1,4 +1,7 @@
 SELECT * from fauna WHERE name LIKE '%fish%';
+
 SELECT * from fauna WHERE avg_age < 21000 AND avg_age > 10000;
+
 SELECT * from fauna WHERE discovery_date IS NULL;
-SELECT * from fauna WHERE discovery_date < '01.01.1950';
+
+SELECT * from fauna WHERE discovery_date < '1950-01-01';
