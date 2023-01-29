c66cf0a705fdc5bb648baa44e3f6c9a65db1f018
SevaStopAll <Sevastopallrh@yandex.ru>
2023-01-29 19:50:53 +0400

    0. Что такое база данных. [#6860 #385690] Changed writing format.

--- db/firstOps.sql
+++ db/firstOps.sql
@@ -1,5 +1,16 @@
-create table students(id serial primary key, name varchar(255),	surname varchar(255), age smallint, count numeric);
-insert into students(name, surname, age) values ('Alex', 'Smith', 25, 2200.0);
+create table students(
+id serial primary key,
+    name varchar(255),
+ 	surname varchar(255),
+ 	age smallint,
+    count numeric);
+
+insert into students
+(name, surname, age)
+values ('Alex', 'Smith', 25, 2200.0);
+
 select * from students;
+
 update students set name = 'Nick';
+
 delete from students;
