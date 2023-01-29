be44811767c15fa6003e731d2ba20d68b808bb01
SevaStopAll <Sevastopallrh@yandex.ru>
2023-01-29 21:40:12 +0400

    2. Группировка и агрегатные функции [#373618 #386058] Table is filled. Corrected my sql scripts.

--- db/devices.sql
+++ db/devices.sql
@@ -1,15 +1,105 @@
+create table devices(
+    id serial primary key,
+    name varchar(255),
+    price float
+);
+
+create table people(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table devices_people(
+    id serial primary key,
+    device_id int references devices(id),
+    people_id int references people(id)
+);
+
+insert into devices(name, price)
+values('Xiaomi 12T', 35000.00);
+
+insert into devices(name, price)
+values('Apple Macbook Air', 89000.00);
+
+insert into devices(name, price)
+values('Nolia 5530', 2350.00);
+
+insert into devices(name, price)
+values('Ipad Air 1', 5000.00);
+
+
+insert into people(name)
+values('Jeff');
+
+insert into people(name)
+values('Collin');
+
+insert into people(name)
+values('Arthur');
+
+insert into people(name)
+values('Linda');
+
+insert into people(name)
+values('Sarah');
+
+insert into people(name)
+values('Amanda');
+
+
+insert into devices_people(device_id, people_id)
+values(1, 1);
+
+insert into devices_people(device_id, people_id)
+values(3, 2);
+
+insert into devices_people(device_id, people_id)
+values(3, 3);
+
+insert into devices_people(device_id, people_id)
+values(1, 4);
+
+insert into devices_people(device_id, people_id)
+values(3, 5);
+
+insert into devices_people(device_id, people_id)
+values(3, 6);
+
+insert into devices_people(device_id, people_id)
+values(2, 1);
+
+insert into devices_people(device_id, people_id)
+values(4, 2);
+
+insert into devices_people(device_id, people_id)
+values(4, 3);
+
+insert into devices_people(device_id, people_id)
+values(2, 4);
+
+insert into devices_people(device_id, people_id)
+values(4, 5);
+
+insert into devices_people(device_id, people_id)
+values(2, 6);
+
+
 select avg(price) from devices;
 
-select devices_people.people_id, avg(devices.price)
+select people.name, avg(devices.price)
 from devices_people
 join devices
 on devices.id = devices_people.device_id
-group by devices_people.people_id
+join people
+on people.id = devices_people.people_id
+group by people.name;
 
 
-select devices_people.people_id, avg(devices.price)
+select people.name, avg(devices.price)
 from devices_people
 join devices
 on devices.id = devices_people.device_id
-group by devices_people.people_id
+join people
+on people.id = devices_people.people_id
+group by people.name;
 having avg(devices.price) > 5000.00;
