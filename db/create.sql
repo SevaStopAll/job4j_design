cedafad14c9d2149c84df7c69caa110327a1fb68
SevaStopAll <Sevastopallrh@yandex.ru>
2023-01-29 20:22:53 +0400

    1. Схема прав пользователи и роли. [#1093 #385909] Correct a lot of errors

--- db/create.sql
+++ db/create.sql
@@ -1,45 +1,23 @@
 create table role(
     id serial primary key,
-    name varchar(250),
+    name varchar(250)
 );
 
 create table rules(
     id serial primary key,
-    name varchar(250),
+    name varchar(250)
 );
 
 create table role_rules(
     id serial primary key,
     role int references role(id),
-    rule int refereneces rules(id)
+    rule int references rules(id)
 );
 
 create table users(
     id serial primary key,
     name varchar(250),
     role int references role(id)
-);
-
-create table item(
-    id serial primary key,
-    name varchar(250),
-    user_id int references users(id);
-    description varchar(250),
-    item_state int refereneces references state(status),
-    item_category int refereneces references category(category)
-);
-
-create table comments(
-    id serial primary key,
-    text varchar(250),
-    request_id int references item(id),
-    user_id int references users(id)
-);
-
-create table attachs(
-    id serial primary key,
-    description varchar(250),
-    item_id int references item(id)
 );
 
 create table category(
@@ -51,5 +29,28 @@ create table state(
     id serial primary key,
     status varchar(250)
 );
+
+create table item(
+    id serial primary key,
+    name varchar(250),
+    user_id int references users(id),
+    description varchar(250),
+    item_state int references state(id),
+    item_category int references category(id)
+);
+
+create table comments(
+    id serial primary key,
+    text varchar(250),
+    item_id int references item(id)
+);
+
+create table attachs(
+    id serial primary key,
+    description varchar(250),
+    item_id int references item(id)
+);
+
+
 
 
--- db/insert.sql
+++ db/insert.sql
@@ -7,14 +7,14 @@ insert into rules(name) values('watch items');
 insert into rules(name) values('modify items');
 insert into rules(name) values('make comments');
 
-insert into role_rules(role, rule) values(1, 1);
-insert into role_rules(role, rule) values(1, 2);
-insert into role_rules(role, rule) values(1, 3);
-insert into role_rules(role, rule) values(1, 4);
-insert into role_rules(role, rule) values(1, 5);
-insert into role_rules(role, rule) values(1, 1);
-insert into role_rules(role, rule) values(1, 3);
-insert into role_rules(role, rule) values(1, 5);
+insert into role_rules(role, rule) values(13, 31);
+insert into role_rules(role, rule) values(13, 32);
+insert into role_rules(role, rule) values(13, 33);
+insert into role_rules(role, rule) values(13, 34);
+insert into role_rules(role, rule) values(13, 35);
+insert into role_rules(role, rule) values(14, 31);
+insert into role_rules(role, rule) values(14, 33);
+insert into role_rules(role, rule) values(14, 35);
 
 insert into state(status) values('sent');
 insert into state(status) values('in work');
@@ -24,12 +24,12 @@ insert into state(status) values('done');
 insert into category(category) values('software');
 insert into category(category) values('hardware');
 
-insert into users(name, role) values('Alexey Ivanov', 1);
-insert into users(name, role) values('Maxim Petrov', 2);
-insert into users(name, role) values('Igor Vasiliev', 2);
+insert into users(name, role) values('Alexey Ivanov', 13);
+insert into users(name, role) values('Maxim Petrov', 14);
+insert into users(name, role) values('Igor Vasiliev', 14);
 
-insert into item(name, user_id, description, item_category, item_state) values ('Micro does not work', 2, 'I can not turn on micro, i have a meet up in 1 hour', 2, 1);
-insert into item(name, user_id, description, item_category, item_state) values ('Cant login in mail', 3, 'My mail does not work correctly, my login is blocked mb', 1, 1);
-insert into attachs(description, item_id) values ('Here is what OS shows in sound menu. There is no micros at all', 1);
-insert into comments(text, request_id, user_id) values ('Did you connect your micro to the PC?', 1, 1);
+insert into item(name, user_id, description, item_category, item_state) values ('Micro does not work', 6, 'I can not turn on micro, i have a meet up in 1 hour', 6, 9);
+insert into item(name, user_id, description, item_category, item_state) values ('Cant login in mail', 7, 'My mail does not work correctly, my login is blocked mb', 5, 9);
+insert into attachs(description, item_id) values ('Here is what OS shows in sound menu. There is no micros at all', 8);
+insert into comments(text, request_id) values ('Did you connect your micro to the PC?', 8);
 
