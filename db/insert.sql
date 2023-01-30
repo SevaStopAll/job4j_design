ffed00cf9119a58e3e9514901f2f005e7ef57a4f
SevaStopAll <Sevastopallrh@yandex.ru>
2023-01-30 11:46:04 +0400

    1. Схема прав пользователи и роли. [#1093 #385909]
    couldn't see some mistakes

--- db/insert.sql
+++ db/insert.sql
@@ -1,35 +1,51 @@
-insert into roles(name) values('admin');
-insert into roles(name) values('user');
+insert into role(name)
+values('admin'), ('user');
 
-insert into rules(name) values('create items');
-insert into rules(name) values('delete items');
-insert into rules(name) values('watch items');
-insert into rules(name) values('modify items');
-insert into rules(name) values('make comments');
+insert into rules(name)
+values('create items'),
+('delete items'),
+('watch items'),
+('modify items'),
+('make comments');
+
 
 insert into role_rules(role, rule) values(13, 31);
+
 insert into role_rules(role, rule) values(13, 32);
+
 insert into role_rules(role, rule) values(13, 33);
+
 insert into role_rules(role, rule) values(13, 34);
+
 insert into role_rules(role, rule) values(13, 35);
+
 insert into role_rules(role, rule) values(14, 31);
+
 insert into role_rules(role, rule) values(14, 33);
+
 insert into role_rules(role, rule) values(14, 35);
 
-insert into state(status) values('sent');
-insert into state(status) values('in work');
-insert into state(status) values('waiting for details');
-insert into state(status) values('done');
+insert into state(status)
+values('sent'), ('in work'), ('waiting for details'), ('done');
 
-insert into category(category) values('software');
-insert into category(category) values('hardware');
+insert into category(category)
+values('software'), ('hardware');
 
-insert into users(name, role) values('Alexey Ivanov', 13);
-insert into users(name, role) values('Maxim Petrov', 14);
-insert into users(name, role) values('Igor Vasiliev', 14);
+insert into users(name, role)
+values('Alexey Ivanov', 13),
+('Maxim Petrov', 14),
+('Igor Vasiliev', 14);
 
-insert into item(name, user_id, description, item_category, item_state) values ('Micro does not work', 6, 'I can not turn on micro, i have a meet up in 1 hour', 6, 9);
-insert into item(name, user_id, description, item_category, item_state) values ('Cant login in mail', 7, 'My mail does not work correctly, my login is blocked mb', 5, 9);
-insert into attachs(description, item_id) values ('Here is what OS shows in sound menu. There is no micros at all', 8);
-insert into comments(text, request_id) values ('Did you connect your micro to the PC?', 8);
+
+insert into item(name, user_id, description, item_category, item_state)
+values ('Micro does not work', 6, 'I can not turn on micro, i have a meet up in 1 hour', 6, 9);
+
+insert into item(name, user_id, description, item_category, item_state)
+values ('Cant login in mail', 7, 'My mail does not work correctly, my login is blocked mb', 5, 9);
+
+insert into attachs(description, item_id)
+values ('Here is what OS shows in sound menu. There is no micros at all', 8);
+
+insert into comments(text, request_id)
+values ('Did you connect your micro to the PC?', 8);
 
