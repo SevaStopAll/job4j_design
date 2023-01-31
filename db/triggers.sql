13a100b013b2288dea6774db8f72dbd96e6dcefb
SevaStopAll <Sevastopallrh@yandex.ru>
2023-01-31 11:18:21 +0400

    2. Триггеры [#504803] Create my first triggers

---
+++ db/triggers.sql
@@ -0,0 +1,84 @@
+create table products (
+    id serial primary key,
+    name varchar(50),
+    producer varchar(50),
+    count integer default 0,
+    price integer
+);
+
+create or replace function discount()
+    returns trigger as
+$$
+    BEGIN
+        update products
+        set price = price - price * 0.2
+        where count <= 5 AND id = new.id;
+        return NEW;
+    END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger discount_trigger
+    after insert
+    on products
+    for each row
+    execute procedure discount();
+
+
+create or replace function tax()
+    returns trigger as
+$$
+    BEGIN
+        update products
+        set price = price * 1.13
+        where id = (select id from inserted);
+        return new;
+    END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger tax_trigger
+    after insert on products
+    referencing new table as inserted
+    for each statement
+    execute procedure tax();
+
+create or replace function tax()
+    returns trigger as
+$$
+    BEGIN
+        update products
+        set price = price * 1.13
+        where id = new.id;
+        return new;
+    END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger tax_before_trigger
+    after insert on products
+    for each row
+    execute procedure tax();
+
+create table history_of_price (
+    id serial primary key,
+    name varchar(50),
+    price integer,
+    date timestamp
+);
+
+create or replace function history()
+    returns trigger as
+$$
+    BEGIN
+       insert into history_of_price(name, price, date)
+       values(new.name, new.price, current_date);
+       return new;
+    END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger history_trigger
+    after insert on products
+    for each row
+    execute procedure history();
