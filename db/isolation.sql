ba8838e097879c522a873d282e3f6330e3563815
SevaStopAll <Sevastopallrh@yandex.ru>
2023-02-01 10:33:29 +0400

    4.3. Транзакции. Уровни изолированности транзакций. [#504812 #386966] change a little my code for provoking transactions' conflict

--- db/isolation.sql
+++ db/isolation.sql
@@ -5,15 +5,19 @@ create table devices(
     count int
 );
 
-postgres=# insert into devices(name, count)
+insert into devices(name, count)
 values('Xiaomi 12T', 10), ('Iphone 12', 7), ('Iphone 13 Pro', 5), ('Nokia 3310', 1), ('Honor 6a', 5);
 
 
 begin transaction isolation level serializable;
 
-update products set count = 10 where name = 'Iphone 12';
+select sum(count) from devices;
 
-update products set count = 20 where name = 'Iphone 13 Pro';
+select sum(count) from devices;
+
+update devices set count = 10 where name = 'Iphone 12';
+
+update devices set count = 20 where name = 'Iphone 13 Pro';
 
 commit;
 
