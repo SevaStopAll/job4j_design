1f7059e7d4244cf7a76b98275e245116c8335a0f
SevaStopAll <Sevastopallrh@yandex.ru>
2023-02-02 09:01:01 +0400

    10. Операторы множества в SQL [#504896] Try to realize union\intersect\except\union all operators in sql

---
+++ db/union_operator.sql
@@ -0,0 +1,42 @@
+CREATE TABLE movie (
+    id SERIAL PRIMARY KEY,
+    name text,
+    director text
+);
+
+CREATE TABLE book (
+    id SERIAL PRIMARY KEY,
+    title text,
+    author text
+);
+
+INSERT INTO movie (name, director)
+VALUES ('Марсианин', 'Ридли Скотт'),
+       ('Матрица', 'Братья Вачовски'),
+       ('Властелин колец', 'Питер Джексон'),
+       ('Гарри Поттер и узник Азкабана', 'Альфонсо Куарон'),
+       ('Железный человек', 'Джон Фавро');
+
+INSERT INTO book (title, author)
+VALUES ('Гарри Поттер и узник Азкабана', 'Джоан Роулинг'),
+       ('Властелин колец', 'Джон Толкин'),
+       ('1984', 'Джордж Оруэлл'),
+       ('Марсианин', 'Энди Уир'),
+       ('Божественная комедия', 'Данте Алигьери');
+
+SELECT name from movie
+intersect
+SELECT title from book;
+
+SELECT title from book
+except
+Select name from movie;
+
+SELECT title from book
+except
+SELECT name from movie
+union
+(select name from movie
+except
+SELECT title from book);
+
