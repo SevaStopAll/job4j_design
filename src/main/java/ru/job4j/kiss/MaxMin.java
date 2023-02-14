0a0e6cc20800fa8c777990f6705460aebe760076
SevaStopAll <Sevastopallrh@yandex.ru>
2023-02-14 16:07:30 +0400

    2. Принципы Kiss, Dry и Yagni [#238813] Hope I could create a well working class with comparators with DRY principe.

--- src/main/java/ru/job4j/kiss/MaxMin.java
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -6,18 +6,23 @@ import java.util.List;
 public class MaxMin {
 
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        T max = null;
-        for (int i = 0; i < value.size(); i++) {
-            if (value.get)
-        }        return null;
+        return count(value, comparator);
     }
 
     public <T> T min(List<T> value, Comparator<T> comparator) {
-        T max = null;
-        for (int i = 0; i < value.size(); i++) {
-
-        }
-        return null;
+        return count(value, comparator);
     }
 
+    private <T> T count(List<T> value, Comparator<T> comparator) {
+        if (value == null) {
+            throw new IllegalArgumentException("List is null");
+        }
+        T target = value.get(0);
+        for (T t : value) {
+            if (comparator.compare(t, target) < 0) {
+                target = t;
+            }
+        }
+        return target;
+    }
 }
--- src/test/java/ru/job4j/kiss/MaxMinTest.java
+++ src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -3,30 +3,55 @@ package ru.job4j.kiss;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.*;
 
 class MaxMinTest {
-
     MaxMin test = new MaxMin();
-
 
     @Test
     void whenFirstIsMin() {
         List<Integer> list = Arrays.asList(4, 6, 5, 7);
         int expected = 4;
-        int result = test.min(list, test.ourComparator);
+        Comparator<Integer> comparator = Integer::compareTo;
+        int result = test.min(list, comparator);
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     void whenFirstIsMax() {
         List<Integer> list = Arrays.asList(7, 5, 6, 3);
         int expected = 7;
-        int result = test.max(list, test.ourComparator);
+        Comparator<Integer> comparator = Comparator.reverseOrder();
+        int result = test.max(list, comparator);
+        assertThat(result).isEqualTo(expected);
     }
 
+    @Test
+    void whenLastIsMax() {
+        List<Integer> list = Arrays.asList(7, 5, 6, 11);
+        int expected = 11;
+        Comparator<Integer> comparator = Comparator.reverseOrder();
+        int result = test.max(list, comparator);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenLastIsMin() {
+        List<Integer> list = Arrays.asList(7, 5, 6, 1);
+        int expected = 1;
+        Comparator<Integer> comparator = Integer::compareTo;
+        int result = test.max(list, comparator);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenListIsNull() {
+        List<Integer> list = null;
+        Comparator<Integer> comparator = Integer::compareTo;
+        assertThatThrownBy(() -> test.max(list, comparator))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
 }
