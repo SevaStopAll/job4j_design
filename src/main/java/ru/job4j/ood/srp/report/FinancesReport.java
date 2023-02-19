dda3b4de9a2ae09d99da731d3e6da898af38203c
SevaStopAll <Sevastopallrh@yandex.ru>
2023-02-19 21:36:08 +0400

    1. Отчеты. [#850 #392495]

--- src/main/java/ru/job4j/ood/srp/report/FinancesReport.java
+++ src/main/java/ru/job4j/ood/srp/report/FinancesReport.java
@@ -7,7 +7,6 @@ import ru.job4j.ood.srp.model.Employee;
 import ru.job4j.ood.srp.store.Store;
 import java.util.Calendar;
 import java.util.function.Predicate;
-
 public class FinancesReport implements Report {
     private final Store store;
     private final DateTimeParser<Calendar> dateTimeParser;
