5dd1c2943a241fb8e081e2476eb4c98ac28e31c3
SevaStopAll <Sevastopallrh@yandex.ru>
2023-02-26 17:14:02 +0400

    2. Парковка машин [#853 #393517] Created tests for SimpleParking class.

--- src/main/java/ru/job4j/ood/lsp/parking/SimpleParking.java
+++ src/main/java/ru/job4j/ood/lsp/parking/SimpleParking.java
@@ -5,6 +5,23 @@ import java.util.List;
 
 public class SimpleParking implements Parking {
     private final int carPlaces;
+
+    public int getFreeCarPlaces() {
+        return freeCarPlaces;
+    }
+
+    public void setFreeCarPlaces(int freeCarPlaces) {
+        this.freeCarPlaces = freeCarPlaces;
+    }
+
+    public List<Auto> getParked() {
+        return parked;
+    }
+
+    public void setParked(List<Auto> parked) {
+        this.parked = parked;
+    }
+
     private int freeCarPlaces;
     private List<Auto> parked;
 
---
+++ src/test/java/ru/job4j/ood/lsp/parking/SimpleParkingTest.java
@@ -0,0 +1,72 @@
+package ru.job4j.ood.lsp.parking;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class SimpleParkingTest {
+    SimpleParking parking = new SimpleParking(10);
+
+    @Test
+    void whenParkOneCar() {
+        parking.parkCar(new Car());
+        assertThat(parking.getFreeCarPlaces()).isEqualTo(9);
+    }
+
+    @Test
+    void whenParkOneTruck() {
+        parking.parkCar(new Truck(2));
+        assertThat(parking.getFreeCarPlaces()).isEqualTo(8);
+    }
+
+    @Test
+    void whenPark11thCar() {
+        parking.parkCar(new Car());
+        parking.parkCar(new Car());
+        parking.parkCar(new Car());
+        parking.parkCar(new Car());
+        parking.parkCar(new Car());
+        parking.parkCar(new Car());
+        parking.parkCar(new Car());
+        parking.parkCar(new Car());
+        parking.parkCar(new Car());
+        parking.parkCar(new Car());
+        assertThat(parking.parkCar(new Car())).isFalse();
+    }
+
+    @Test
+    void whenPark10thTruck() {
+        parking.parkCar(new Car());
+        parking.parkCar(new Car());
+        parking.parkCar(new Car());
+        parking.parkCar(new Car());
+        parking.parkCar(new Car());
+        parking.parkCar(new Car());
+        parking.parkCar(new Car());
+        parking.parkCar(new Car());
+        parking.parkCar(new Car());
+        assertThat(parking.parkCar(new Truck(2))).isFalse();
+    }
+
+    @Test
+    void whenParkTwoCarAndTwoTrucks() {
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Truck truck1 = new Truck(2);
+        Truck truck2 = new Truck(2);
+        List<Auto> list = new ArrayList<>();
+        list.add(car1);
+        list.add(car2);
+        list.add(truck1);
+        list.add(truck2);
+        parking.parkCar(car1);
+        parking.parkCar(car2);
+        parking.parkCar(truck1);
+        parking.parkCar(truck2);
+        assertThat(parking.getParked()).isEqualTo(list);
+    }
+}
