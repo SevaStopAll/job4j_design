f1dabfc0e1758eb0fc45c5f21aa07afad81c5ac6
SevaStopAll <Sevastopallrh@yandex.ru>
2023-02-28 18:49:27 +0400

    1. Создать меню. [#4748] Realized print method, but I have to watch it one more time.

--- src/main/java/ru/job4j/ood/isp/menu/SimpleMenuPrinter.java
+++ src/main/java/ru/job4j/ood/isp/menu/SimpleMenuPrinter.java
@@ -1,20 +1,29 @@
 package ru.job4j.ood.isp.menu;
 
 public class SimpleMenuPrinter implements MenuPrinter {
+    private final String divider = " ";
     @Override
     public void print(Menu menu) {
-        menu.forEach(i -> System.out.println(i.getNumber() + i.getName()));
+        menu.forEach(i -> {
+            String number = i.getNumber();
+            String repeated = "----".repeat(i.getNumber().length() / 2 - 1);
+            if (number.length() == 2) {
+                System.out.println(i.getName() + divider + number);
+            } else {
+                System.out.println(repeated + divider + i.getName() + divider + number);
+            }
+        });
     }
 
-/*    Задача 1.
-
-            ---- Задача 1.1.
-
-            --------- Задача 1.1.1.
-
-            --------- Задача 1.1.2.
-
-            ----- Задача 1.2.*/
-
-
+    public static void main(String[] args) {
+        Menu menu = new SimpleMenu();
+        final ActionDelegate STUB_ACTION = System.out::println;
+        menu.add(Menu.ROOT, "Сходить в магазин", STUB_ACTION);
+        menu.add(Menu.ROOT, "Покормить собаку", STUB_ACTION);
+        menu.add("Сходить в магазин", "Купить продукты", STUB_ACTION);
+        menu.add("Купить продукты", "Купить хлеб", STUB_ACTION);
+        menu.add("Купить продукты", "Купить молоко", STUB_ACTION);
+        SimpleMenuPrinter printer = new SimpleMenuPrinter();
+        printer.print(menu);
+    }
 }
